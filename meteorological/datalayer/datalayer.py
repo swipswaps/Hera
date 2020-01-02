35e1530019d046e2a2f1950025fc1f12ac478fba
eden <edenn@iibr.gov.il>
2020-01-02 15:28:05 +0200

    - Added in getTurbulenceCalculator function, the option to put start or end as string.

--- meteorological/datalayer/datalayer.py
+++ meteorological/datalayer/datalayer.py
@@ -1,5 +1,6 @@
 from ..analytics.turbulencecalculator import TurbulenceCalculator
 from ... import datalayer
+import pandas
 
 def getTurbulenceCalculator(projectName, samplingWindow, start=None, end=None, usePandas=None, isMissingData=False, **kwargs):
     """
@@ -16,6 +17,12 @@ def getTurbulenceCalculator(projectName, samplingWindow, start=None, end=None, u
     :return: A turbulence calculator of the loaded raw data.
     """
     projectData = datalayer.Project[projectName]
+
+    if type(start) is str:
+        start = pandas.Timestamp(start)
+
+    if type(end) is str:
+        end = pandas.Timestamp(end)
 
     if start is None or end is None:
         start = projectData['start']
