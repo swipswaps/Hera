674907b2afe4052a4b17f1cc4d65da7de85447aa
yehudaa <yehudaa@iibr.gov.il>
2020-03-26 16:52:10 +0200

    0.1.1 Removed the necessity to have a public db

--- hera/__init__.py
+++ hera/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .measurements import meteorological as meteo
 from .simulations import LSM
@@ -7,6 +7,10 @@ from .measurements import GIS
 
 
 """
+ 0.1.1
+ -----
+ Removed the necessity to have a public DB
+
  0.1.0
  -----
  getData() from datalayer returns list of data.
