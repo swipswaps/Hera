709adcb19569617150e0e8e35041718b11393ba9
eden <edenn@iibr.gov.il>
2020-04-02 10:34:52 +0300

    - Changed bin/hera-datalayer.py to work with function handler and added loadTo and loadFrom options. - Added the option to put the column names in the functions of Simulation/analysis/errorCalculation.py

--- hera/bin/hera-datalayer.py
+++ hera/bin/hera-datalayer.py
@@ -3,36 +3,79 @@ import json
 from pyhera.hera import datalayer
 
 parser = argparse.ArgumentParser()
-parser.add_argument('action', nargs=1, type=str)
-parser.add_argument('projectName', nargs=1, type=str)
-parser.add_argument('query', nargs='*', type=str)
+parser.add_argument('command', nargs=1, type=str)
+parser.add_argument('args', nargs='*', type=str)
 
 args = parser.parse_args()
 
-action = args.action[0]
-projectName = args.projectName[0]
-query = args.query
 
-fullQuery = dict(projectName=projectName)
-for queryElement in query:
-    fullQuery[queryElement.split('=')[0]] = eval(queryElement.split('=')[1])
-
-if action=='list':
+def list_handler(arguments):
+    fullQuery={}
+    for queryElement in arguments:
+        fullQuery[queryElement.split('=')[0]] = eval(queryElement.split('=')[1])
     for doc in datalayer.Measurements.getDocuments(**fullQuery):
         print(doc.asDict())
-    #print('list')
-elif action=='load':
+
+
+def load_handler(arguments):
+    fullQuery = {}
+    for queryElement in arguments:
+        fullQuery[queryElement.split('=')[0]] = eval(queryElement.split('=')[1])
     datalayer.Measurements.addDocument(**fullQuery)
-    #print('load')
-elif action=='delete':
+
+
+def delete_handler(arguments):
     try:
-        with open(projectName, 'r') as myFile:
+        with open(arguments[0], 'r') as myFile:
             docList = json.load(myFile)['documents']
         for doc in docList:
             for key in doc['_id']:
                 id = doc['_id'][key]
             datalayer.Measurements.deleteDocumentByID(id=id)
     except FileNotFoundError:
+        fullQuery = {}
+        for queryElement in arguments:
+            fullQuery[queryElement.split('=')[0]] = eval(queryElement.split('=')[1])
         with open('docToDelete.json', 'w') as myFile:
             json.dump(datalayer.Measurements.getDocumentsAsDict(**fullQuery, with_id=True), myFile, indent=4, sort_keys=True)
-    #print('delete')
+
+
+def loadTo_handler(arguments):
+    info = arguments[0]
+    mongoConfig = dict(username=info.split(':')[0],
+                       password=info.split(':')[1].split('@')[0],
+                       dbIP=info.split(':')[1].split('@')[1].split('/')[0],
+                       dbName=info.split(':')[1].split('@')[1].split('/')[1]
+                       )
+
+    datalayer.createDBConnection('other', mongoConfig=mongoConfig)
+
+    fullQuery = {}
+    for queryElement in arguments[1:]:
+        fullQuery[queryElement.split('=')[0]] = eval(queryElement.split('=')[1])
+
+    docList = datalayer.Measurements.getDocuments(**fullQuery)
+    for doc in docList:
+        datalayer.Measurements_Collection(user='other').addDocument(**doc.asDict())
+
+
+def loadFrom_handler(arguments):
+    info = arguments[0]
+    mongoConfig = dict(username=info.split(':')[0],
+                       password=info.split(':')[1].split('@')[0],
+                       dbIP=info.split(':')[1].split('@')[1].split('/')[0],
+                       dbName=info.split(':')[1].split('@')[1].split('/')[1]
+                       )
+
+    datalayer.createDBConnection('other', mongoConfig=mongoConfig)
+
+    fullQuery = {}
+    for queryElement in arguments[1:]:
+        fullQuery[queryElement.split('=')[0]] = eval(queryElement.split('=')[1])
+
+    docList = datalayer.Measurements_Collection(user='other').getDocuments(**fullQuery)
+    for doc in docList:
+        datalayer.Measurements.addDocument(**doc.asDict())
+
+
+globals()['%s_handler' % args.command[0]](args.args)
