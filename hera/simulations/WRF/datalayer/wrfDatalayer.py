859ad797a32dfa928826f0b43a4f91c0112e9498
ofir <ofir@iibr.com>
2020-04-30 16:03:09 +0300

    finished building a function that gets an hour of the day, a path of wrf results and longtitude/latitude ITM coordinates and returns a pandas dataframe with the results. Still has a bug in the calculation of the heights. There is a ~20 m difference between two methods of calculations.

--- hera/simulations/WRF/datalayer/wrfDatalayer.py
+++ hera/simulations/WRF/datalayer/wrfDatalayer.py
@@ -7,61 +7,129 @@ import xarray
 
 class wrfDatalayer():
 
-    def getPandas(self, datapath, timelist, lat=None, lon=None, heightLimit=None):
+    def find_i(self, request, xdata, dimension, coordinate):
+        """
+        Finds the i value of a dimension that may interpolate a requested value of a coordinate,
+        for example, dimension west_east for coordinate XLONG
+
+        Parameters
+        ----------
+        request: The requested value
+        xdata: An xarray with the data
+        dimension: The dimension, for example, west_east
+        coordinate: The coordinate, for example, XLONG
+
+        Returns: The value that may be used for an interpolation.
+        -------
+
+        """
+
+        for i in range(len(xdata.__getattr__(dimension))):
+            compare_dict = {dimension: i}
+            compare_dictplus = {dimension: i + 1}
+            if request > float(xdata.isel(Time=0, bottom_top=0, **compare_dict).__getattr__(coordinate).min()) - 0.01 and\
+               request < float(xdata.isel(Time=0, bottom_top=0, **compare_dict).__getattr__(coordinate).max()) + 0.01:
+                delta = float(xdata.isel(Time=0, bottom_top=0, **compare_dictplus).__getattr__(coordinate).mean()) - \
+                        float(xdata.isel(Time=0, bottom_top=0, **compare_dict).__getattr__(coordinate).mean())
+                request_delta = request - float(xdata.isel(Time=0, bottom_top=0, **compare_dict).__getattr__(coordinate).mean())
+                break
+
+        return i + request_delta / delta
+
+    def getPandas(self, datapath, Time=None, lat=None, lon=None, heightLimit=None, compare_lat=650000, compare_lon=200000):
+        """
+        Makes a pandas dataframe that holds data of wrf simulation.
+        Parameters
+        ----------
+        datapath: The path of the wrf results.
+        Time: A datetime.time object with time in hours and minutes, or None for all times.
+        lat: An ITM latitude coordinate. Either one of lat or lan must be given.
+        lon: An ITM latitude coordinate. Either one of lat or lan must be given.
+        heightLimit: An optional height limit
+        compare_lat: A coordinate that is used to get the wpf-84 coordinates for the function's operation.
+        compare_lon: A coordinate that is used to get the wpf-84 coordinates for the function's operation.
+
+        Returns: A pandas dataframe.
+        -------
+
+        """
 
         d = pandas.DataFrame()
         data = Dataset(datapath)
         xdata = xarray.open_dataset(datapath)
-        vals = []
+
+        if Time is not None:
+            pStart = pandas.Timestamp(wrf.getvar(data, "times").values)
+            pTime = pandas.Timestamp(year=pStart.year, month=pStart.month, day=pStart.day, hour=Time.hour, minute=Time.minute)
+            for i in range(len(xdata.Time)):
+                if pTime >= wrf.getvar(data, "times", timeidx=i).values and  pTime <= wrf.getvar(data, "times", timeidx=i+1).values:
+                    delta = (wrf.getvar(data, "times", timeidx=i+1).values-wrf.getvar(data, "times", timeidx=i).values)/numpy.timedelta64(1, 's')
+                    request_delta = (pTime - wrf.getvar(data, "times", timeidx=i).values).total_seconds()
+                    request_i_time = [i + request_delta/delta]
+                    break
+        else:
+            request_i_time = [x for x in range(len(xdata.Time))]
+
         if lat is not None:
+            geo = geopandas.GeoDataFrame(dict(geometry=geopandas.points_from_xy([compare_lon],[lat])),index=[0])
+            geo.crs = 2039
+            geo = geo.to_crs(epsg=4326)
+            lat = geo.geometry[0].y
             changes = ["south_north", "south_north", "south_north_stag"]
-            l = wrf.getvar(data, "lat")
-            for i in range(len(xdata.south_north)):
-                if lat > float(l.isel(south_north=i).min())-0.01 and lat < float(l.isel(south_north=i).max())+0.01:
-                    vals.append(i)
+            request_i_u = request_i = self.find_i(lat, xdata, "south_north", "XLAT")
+            request_i_v = self.find_i(lat, xdata, "south_north_stag", "XLAT_V")
+
         elif lon is not None:
+            geo = geopandas.GeoDataFrame(dict(geometry=geopandas.points_from_xy([lon],[compare_lat])),index=[0])
+            geo.crs = 2039
+            geo = geo.to_crs(epsg=4326)
+            lon = geo.geometry[0].x
             changes = ["west_east", "west_east_stag", "west_east"]
-            l = wrf.getvar(data, "lon")
-            for i in range(len(xdata.west_east)):
-                if lon > float(l.isel(west_east=i).min())-0.01 and lon < float(l.isel(west_east=i).max())+0.01:
-                    vals.append(i)
+            request_i_v = request_i = self.find_i(lon, xdata, "west_east", "XLONG")
+            request_i_u = self.find_i(lon, xdata, "west_east_stag", "XLONG_U")
+        else:
+            raise KeyError("Choose longtitude or latitude.")
 
         H_vals = []
         if heightLimit is not None:
-            for val in vals:
-                query_dict = {changes[0]: val}
-                ht = wrf.getvar(data, "z").isel(**query_dict)
-                for i in range(len(xdata.bottom_top)):
-                    if ht.isel(bottom_top=i).min()<heightLimit:
-                        H_vals.append(i)
+            query_dict = {changes[0]: request_i}
+            ht = wrf.getvar(data, "z")
+            for i in range(len(xdata.bottom_top)):
+                if ht.isel(bottom_top=i).interp(**query_dict).min()<heightLimit:
+                    H_vals.append(i)
+        else:
+            H_vals.append(len(xdata.bottom_top))
 
-        for i in timelist:
-            ht = wrf.getvar(data, "z", units="m", timeidx=i)
-            U = wrf.getvar(data, "U", timeidx=i)
-            V = wrf.getvar(data, "V", timeidx=i)
-            ter = wrf.getvar(data, "ter", timeidx=i)
-            time = wrf.getvar(data, "times", timeidx=i)
+        for i in request_i_time:
+
+            ter = wrf.getvar(data, "ter", timeidx=int(i))
+
             for j in range(max(H_vals)):
-                for val in vals:
-                    query_dict = {changes[0]: val}
-                    query_dictU = {changes[1]: val}
-                    query_dictV = {changes[2]: val}
-                    if lat is not None:
-                        new_u = U.isel(bottom_top=j, **query_dictU)[:-1]
-                        new_v = V.isel(bottom_top=j, **query_dictV)
-                    elif lon is not None:
-                        new_u = U.isel(bottom_top=j, **query_dictU)
-                        new_v = V.isel(bottom_top=j, **query_dictV)[:-1]
-                    new_d = pandas.DataFrame(dict(height=ht.isel(bottom_top=j, **query_dict),
-                                                  U=new_u,
-                                                  V=new_v,
-                                                  Time=time,
-                                                  terrain=ter.isel(**query_dict),
-                                                  LAT=xdata.isel(bottom_top=j, **query_dict,
-                                                                 Time=i).XLAT,
-                                                  LONG=xdata.isel(bottom_top=j, **query_dict,
-                                                                  Time=i).XLONG))
-                    d = pandas.concat([d, new_d])
+
+                query_dict = {changes[0]: request_i}
+                query_dictU = {changes[1]: request_i_u}
+                query_dictV = {changes[2]: request_i_v}
+                xnewdata = xdata.isel(bottom_top=j).interp(**query_dict, Time=i)
+                if lat is not None:
+                    new_u = xdata.isel(bottom_top=j).interp(**query_dictU, Time=i).U[:-1]
+                    new_v = xdata.isel(bottom_top=j).interp(**query_dictV, Time=i).V
+                elif lon is not None:
+                    new_u = xdata.isel(bottom_top=j).interp(**query_dictU, Time=i).U
+                    new_v = xdata.isel(bottom_top=j).interp(**query_dictV, Time=i).V[:-1]
+                if Time is None:
+                    new_t = wrf.getvar(data, "time", timeidx=i).values
+                else:
+                    new_t = pTime
+
+                new_d = pandas.DataFrame(dict(height=(xnewdata.isel(bottom_top_stag=j).PH+xnewdata.isel(bottom_top_stag=j).PHB)/9.81,
+                                              height2 = wrf.getvar(data, "z", timeidx=int(i)).interp(bottom_top=j,**query_dict),
+                                              U=new_u,
+                                              V=new_v,
+                                              Time=new_t,
+                                              terrain=ter.interp(**query_dict),
+                                              LAT=xnewdata.XLAT,
+                                              LONG=xnewdata.XLONG))
+                d = pandas.concat([d, new_d])
 
         gdf = geopandas.GeoDataFrame(d, geometry=geopandas.points_from_xy(d.LONG, d.LAT))
         gdf.crs = 4326
