7aed96d1ecfb7b20d9944d96fa6bcb64b57820e6
mattyab <mattya@mattya>
2020-06-07 14:23:10 +0300

    changes while working on the laptop (Mattya)

--- hera/measurements/meteorological/analytics/turbulencecalculator.py
+++ hera/measurements/meteorological/analytics/turbulencecalculator.py
@@ -893,6 +893,35 @@ class TurbulenceCalculator(AbstractCalculator):
     def StrucFunDir(self, tau_range = None, dir1_data = None, u_dir1 = "u_dir1", v_dir1 = "v_dir1", w_dir1 = "w_dir1",
                     dir2_data = None, u_dir2 = "u_dir2", v_dir2 = "v_dir2", w_dir2 = "w_dir2", title = "", inMemory = None):
 
+        """
+        Calculates the 2nd order structure function <(ui(t+tau)-ui(t)) * (uj(t+tau)-uj(t))> for a given list of tau values,
+        on given directions for ui, uj (which might be different for each temporal window).
+
+        :param tau_range: list of tau values (floats)
+
+        :param dir1_data: pandas dataframe containing the direction vectors on which ui is calculated (index not necessarily
+                            with the same frequency as the sampling window, but the sampling window is assumed to divide
+                            the index frequency of dir1_data), vectors not necesarily normalized
+
+        :param u_dir1: str name of column in dir1_data containing the x direction of the direction vectors
+
+        :param v_dir1: str name of column in dir1_data containing the y direction of the direction vectors
+
+        :param w_dir1: str name of column in dir1_data containing the z direction of the direction vectors
+
+        :param dir2_data: same for uj - if not given, same directions as in dir1_data will be used
+
+        :param u_dir2: same for uj
+
+        :param v_dir2: same for uj
+
+        :param w_dir2: same for uj
+
+        :param title: title to add for the structure function column in the returned data (see col_names below)
+
+        :param inMemory:
+        :return:
+        """
         if self._InMemoryAvgRef is None:
             self._InMemoryAvgRef = inMemory
 
@@ -904,90 +933,115 @@ class TurbulenceCalculator(AbstractCalculator):
 
         col_names = {tau:"D" + title + "_" + str(tau) + "s" for tau in tau_range}
         if set(col_names.values()).issubset(set(self._TemporaryData.columns)):
-            # self._TemporaryData = self._TemporaryData.loc[(self._TemporaryData.index >= self.Identifier["start"]) &
-            #                                               (self._TemporaryData.index < self.Identifier["end"])]
             return self
 
         self.fluctuations()
 
-        # Extracting ...
+        # Extracting the supplied direction data of ui,uj:
         dir1_data_new = dir1_data[[u_dir1,v_dir1,w_dir1]]\
                        .rename(columns = {u_dir1: "u_dir1", v_dir1: "v_dir1", w_dir1: "w_dir1"})\
                        .loc[(dir1_data.index >= self.Identifier["start"]) & (dir1_data.index < self.Identifier["end"])]
 
-        # Computing ...
+        dir2_data_new = dir2_data[[u_dir2, v_dir2, w_dir2]] \
+            .rename(columns={u_dir2: "u_dir2", v_dir2: "v_dir2", w_dir2: "w_dir2"}) \
+            .loc[(dir2_data.index >= self.Identifier["start"]) & (dir2_data.index < self.Identifier["end"])]
+
+        # Computing the magnitude of the direction vectors and normalizing them:
         dir1_data_new["dir1_mag"] = (dir1_data_new["u_dir1"] ** 2 + dir1_data_new["v_dir1"] ** 2 + dir1_data_new["w_dir1"] ** 2) ** 0.5
 
         dir1_data_new.loc[:,['u_dir1','v_dir1','w_dir1']] = dir1_data_new.loc[:,['u_dir1','v_dir1','w_dir1']].div(dir1_data_new["dir1_mag"], axis=0)
 
         dir1_data_new = dir1_data_new.drop(columns = 'dir1_mag')
 
-        dir2_data_new = dir2_data[[u_dir2,v_dir2,w_dir2]]\
-                       .rename(columns = {u_dir2: "u_dir2", v_dir2: "v_dir2", w_dir2: "w_dir2"})\
-                       .loc[(dir2_data.index >=self.Identifier["start"]) & (dir2_data.index < self.Identifier["end"])]
-
         dir2_data_new["dir2_mag"] = (dir2_data_new["u_dir2"] ** 2 + dir2_data_new["v_dir2"] ** 2 + dir2_data_new["w_dir2"] ** 2) ** 0.5
 
         dir2_data_new.loc[:,['u_dir2','v_dir2','w_dir2']] = dir2_data_new.loc[:,['u_dir2','v_dir2','w_dir2']].div(dir2_data_new["dir2_mag"], axis=0)
 
         dir2_data_new = dir2_data_new.drop(columns = 'dir2_mag')
-        # u_mag_new = u_magnitude_data.rename("u_mag").loc[(u_magnitude_data.index >=  self.Identifier["start"]) &
-        #                         (u_magnitude_data.index < self.Identifier["end"])]
 
-        to_drop = set(self._RawData.columns) & {"u_dir1", "v_dir1", "w_dir1", "u_dir2", "v_dir2", "w_dir2"}
-        if bool(to_drop):
-            self._RawData = self._RawData.drop(columns = to_drop)
-
-
+        # Creating the temporary raw data on which calculations are performed (u,v,w + direction data):
         united_data = self._RawData.merge(dir1_data_new, how = "outer", left_index = True, right_index = True)\
                            .merge(dir2_data_new, how = "outer", left_index = True, right_index = True)\
                            .dropna(how='all')
 
-        united_data[["u_dir1", "v_dir1", "w_dir1", "u_dir2", "v_dir2", "w_dir2"]].ffill().dropna(how='any')
+        united_data[["u_dir1", "v_dir1", "w_dir1", "u_dir2", "v_dir2", "w_dir2"]] = \
+            united_data[["u_dir1", "v_dir1", "w_dir1", "u_dir2", "v_dir2", "w_dir2"]].ffill()
 
+        united_data = united_data.dropna(how='any')
+
+        # Computing ui,uj as projections of u,v,w on the direction vectors:
         united_data["ui"] = 0
         united_data["uj"] = 0
         for component in ["u","v","w"]:
-            united_data["ui"] += united_data[component]*united_data["%s_dir1" % component]
+            united_data["ui"] += united_data[component] * united_data["%s_dir1" % component]
             united_data["uj"] += united_data[component] * united_data["%s_dir2" % component]
 
-        #print("RawData",[self._RawData.get_partition(n) for n in range(self._RawData.npartitions)])
         for tau in tau_range:
+            # Computation of <(ui(t+tau)-ui(t)) * (uj(t+tau)-uj(t))>:
             if col_names[tau] not in self._TemporaryData.columns:
+                # Computing the data of ui,uj(t+tau):
                 data_tau = united_data[["ui","uj"]].reset_index()
                 data_tau["Time"] -= pandas.Timedelta(tau, unit="s")
-
                 data_tau = data_tau.set_index("Time").rename(columns = {"ui":"ui_shifted","uj":"uj_shifted"})
-                print("data_tau", [data_tau.get_partition(n) for n in range(data_tau.npartitions)])
+
+                # Drop potentially used columns from last tau:
                 to_drop = set(united_data.columns) & {"ui_shifted", "uj_shifted"}
                 if bool(to_drop):
                     united_data = united_data.drop(columns = to_drop)
-                united_data = united_data.merge(data_tau, how = "left", left_index = True, right_index = True).repartition(freq = "1D")
 
-                #print("RawData", [self._RawData.get_partition(n) for n in range(self._RawData.npartitions)])
-
+                # Merge data(t) with data(t+tau), repartition (otherwise gets crazy):
+                united_data = united_data.merge(data_tau, how = "left", left_index = True, right_index = True).repartition(freq = "1W")
+                # Final calculation:
                 self._TemporaryData[col_names[tau]] = ((united_data["ui_shifted"] - united_data["ui"]) *\
                                                  (united_data["uj_shifted"] - united_data["uj"])).resample(self.SamplingWindow).mean()
                 self._CalculatedParams.append([col_names[tau],{}])
 
-        # self._TemporaryData = self._TemporaryData.loc[(self._TemporaryData.index >= self.Identifier["start"]) &
-        #                                               (self._TemporaryData.index < self.Identifier["end"])]
         return self
 
     def StrucFun(self, tau_range = None, ubar_data = None, u_bar = "u_bar", v_bar = "v_bar", w_bar = "w_bar",
                      mode = "MeanDir", title_additions = "", inMemory = None):
 
+        """
+        Calculates (at least 1 component of) the 2nd order structure function <(ui(t+tau)-ui(t)) * (uj(t+tau)-uj(t))>
+        for a given list of tau values, with axes aligned with the 3d mean velocity direction.
+
+        :param tau_range: range of tau values
+
+        :param ubar_data: pandas dataframe of externally supplied mean velocity data. Default is the same mean velocity
+                            computed using fluctuations. The frequency of ubar_data.index is assumed to be divisible by
+                            the sampling window.
+
+        :param u_bar: str name of column in ubar_data that contains the x component of the mean velocity
+
+        :param v_bar: str name of column in ubar_data that contains the y component of the mean velocity
+
+        :param w_bar: str name of column in ubar_data that contains the z component of the mean velocity
+
+        :param mode: either default "MeanDir" or "3dMeanDir"
+                        MeanDir - calculate only D11, where the 1 component is aligned with the mean velocity 3d direction
+                        3dMeanDir - calculate all 6 components Dij, i,j=1,2,3, where the axes are defined as:
+                                    1 - mean velocity direction
+                                    2 - a horizontal direction perpendicular to the mean velocity (z cross mean vel dir)
+                                    3 - a direction perpendicular to 1,2 (dir 1 cross dir 2)
+
+        :param title_additions: str additions to the column titles of the resulting computations
+
+        :param inMemory:
+        :return:
+        """
         if self._InMemoryAvgRef is None:
             self._InMemoryAvgRef = inMemory
 
         if ubar_data is None:
+            # Default ubar_data
             self.fluctuations()
             ubar_data = self._TemporaryData[["u_bar","v_bar","w_bar"]].compute()
 
         if mode == "MeanDir":
             self.StrucFunDir(tau_range=tau_range, dir1_data=ubar_data, u_dir1=u_bar, v_dir1=v_bar, w_dir1=w_bar, title = "11" + title_additions)
 
-        if mode == "3dMeanDir":
+        elif mode == "3dMeanDir":
+            # Create direction data for all 3 directions (directions are not normalized since StrucFunDir takes care of that)
             ubar_new = ubar_data[[u_bar,v_bar,w_bar]].rename(columns = {u_bar:"x_hat1",v_bar:"x_hat2",w_bar:"x_hat3"})
             ubar_new["y_hat1"] = - ubar_new["x_hat2"]
             ubar_new["y_hat2"] = ubar_new["x_hat1"]
@@ -996,6 +1050,7 @@ class TurbulenceCalculator(AbstractCalculator):
             ubar_new["z_hat2"] = - ubar_new["x_hat2"] * ubar_new["x_hat3"]
             ubar_new["z_hat3"] = ubar_new["x_hat1"] ** 2 +  ubar_new["x_hat2"] ** 2
 
+            # Compute for each ij
             self.StrucFunDir(tau_range=tau_range, dir1_data=ubar_new, u_dir1="x_hat1", v_dir1="x_hat2", w_dir1="x_hat3", title="11" + title_additions)
             self.StrucFunDir(tau_range=tau_range, dir1_data=ubar_new, u_dir1="y_hat1", v_dir1="y_hat2", w_dir1="y_hat3", title="22" + title_additions)
             self.StrucFunDir(tau_range=tau_range, dir1_data=ubar_new, u_dir1="z_hat1", v_dir1="z_hat2", w_dir1="z_hat3", title="33" + title_additions)
@@ -1005,6 +1060,8 @@ class TurbulenceCalculator(AbstractCalculator):
                              dir2_data = ubar_new, u_dir2="z_hat1", v_dir2="z_hat2", w_dir2="z_hat3",title="13" + title_additions)
             self.StrucFunDir(tau_range=tau_range, dir1_data=ubar_new, u_dir1="y_hat1", v_dir1="y_hat2", w_dir1="y_hat3",
                              dir2_data = ubar_new, u_dir2="z_hat1", v_dir2="z_hat2", w_dir2="z_hat3",title="23" + title_additions)
+        else:
+            raise("mode must be either MeanDir or 3dMeanDir")
 
         self._TemporaryData["u_mag" + title_additions] = ((ubar_data[u_bar] ** 2 + ubar_data[v_bar] ** 2 + ubar_data[w_bar] ** 2) ** 0.5).loc[(ubar_data.index >=
                                 self.Identifier["start"]) & (ubar_data.index < self.Identifier["end"])]
@@ -1053,52 +1110,82 @@ class TurbulenceCalculator(AbstractCalculator):
     def ThirdStrucFun(self, tau_range = None, ubar_data = None, u_bar = "u_bar", v_bar = "v_bar", w_bar = "w_bar",
                      title_additions = "", inMemory = None):
 
+        """
+        Calculates the 3nd order structure function <(u1(t+tau)-u1(t))^3> for a given list of tau values, where u1 is the
+        velocity component along the mean velocity direction (which might be different for each temporal window).
+
+        :param tau_range: list of tau values (floats)
+
+        :param ubar_data: pandas dataframe of externally supplied mean velocity data. Default is the same mean velocity
+                            computed using fluctuations. The frequency of ubar_data.index is assumed to be divisible by
+                            the sampling window.
+
+        :param u_bar: str name of column in ubar_data that contains the x component of the mean velocity
+
+        :param v_bar: str name of column in ubar_data that contains the y component of the mean velocity
+
+        :param w_bar: str name of column in ubar_data that contains the z component of the mean velocity
+
+        :param title_additions: str additions to the column titles of the resulting computations
+
+        :param inMemory:
+
+        :return:
+        """
+
         if self._InMemoryAvgRef is None:
             self._InMemoryAvgRef = inMemory
 
         self.fluctuations()
         if ubar_data is None:
+            # Default ubar_data
             ubar_data = self._TemporaryData[["u_bar","v_bar","w_bar"]].compute()
 
         col_names = {tau:"D111" + title_additions + "_" + str(tau) + "s" for tau in tau_range}
         if set(col_names.values()).issubset(set(self._TemporaryData.columns)):
-            # self._TemporaryData = self._TemporaryData.loc[(self._TemporaryData.index >= self.Identifier["start"]) &
-            #                                               (self._TemporaryData.index < self.Identifier["end"])]
             return self
 
+        # Extracting the supplied mean velocity data:
         dir_data = ubar_data[[u_bar,v_bar,w_bar]].rename(columns={u_bar: "u_dir", v_bar: "v_dir", w_bar: "w_dir"}).loc[
             (ubar_data.index >=self.Identifier["start"]) & (ubar_data.index < self.Identifier["end"])]
+
+        # Computing the velocity magnitude and normalizing the velocity vectors:
         dir_data["u_mag"] = (dir_data["u_dir"] ** 2 + dir_data["v_dir"] ** 2 + dir_data["w_dir"] ** 2) ** 0.5
         dir_data.loc[:, ['u_dir', 'v_dir', 'w_dir']] = dir_data.loc[:, ['u_dir', 'v_dir', 'w_dir']].div(dir_data["u_mag"], axis=0)
 
-        to_drop = set(self._RawData.columns) & {"u_dir", "v_dir", "w_dir", "u1_shifted"}
-        if bool(to_drop):
-            self._RawData = self._RawData.drop(columns = to_drop)
-        self._RawData = self._RawData.merge(dir_data[['u_dir', 'v_dir', 'w_dir']], how="outer", left_index=True, right_index=True)
-        self._RawData = self._RawData.dropna(how='all')
-        self._RawData[["u_dir", "v_dir", "w_dir"]] = self._RawData[["u_dir", "v_dir", "w_dir"]].ffill()
-        self._RawData = self._RawData.dropna(how='any')
+        # Creating the temporary raw data on which calculations are performed (u,v,w + direction data):
+        united_data = self._RawData.merge(dir_data[['u_dir', 'v_dir', 'w_dir']], how="outer", left_index=True, right_index=True)\
+                .dropna(how='all')
+        united_data[["u_dir", "v_dir", "w_dir"]] = united_data[["u_dir", "v_dir", "w_dir"]].ffill()
+        united_data = united_data.dropna(how='any')
 
-        self._RawData["u1"] = self._RawData["u"] * self._RawData["u_dir"] + self._RawData["v"] * self._RawData["v_dir"] + \
-                              self._RawData["w"] * self._RawData["w_dir"]
+        # Computing u1 as a projection of u,v,w on the direction vectors:
+        united_data["u1"] = 0
+        for component in ["u", "v", "w"]:
+            united_data["u1"] += united_data[component] * united_data["%s_dir" % component]
 
         for tau in tau_range:
+            # Computation of <(u1(t+tau)-u1(t))^3>:
             if col_names[tau] not in self._TemporaryData.columns:
-                data_tau = self._RawData[["u1"]].reset_index()
+                # Computing the data of u1(t+tau):
+                data_tau = united_data[["u1"]].reset_index()
                 data_tau["Time"] -= pandas.Timedelta(tau, unit="s")
                 data_tau = data_tau.set_index("Time").rename(columns = {"u1":"u1_shifted"})
-                if "u1_shifted" in self._RawData.columns:
-                    self._RawData = self._RawData.drop(columns = "u1_shifted")
-                self._RawData = self._RawData.merge(data_tau, how = "left", left_index = True, right_index = True)
-                self._TemporaryData[col_names[tau]] = ((self._RawData["u1_shifted"] - self._RawData["u1"]) ** 3).resample(self.SamplingWindow).mean()
+
+                # Drop potentially used columns from last tau:
+                if "u1_shifted" in united_data.columns:
+                    united_data = united_data.drop(columns = "u1_shifted")
+
+                # Merge data(t) with data(t+tau), repartition (otherwise gets crazy):
+                united_data = united_data.merge(data_tau, how = "left", left_index = True, right_index = True).repartition(freq = "1W")
+                # Final calculation:
+                self._TemporaryData[col_names[tau]] = ((united_data["u1_shifted"] - united_data["u1"]) ** 3)\
+                                                        .resample(self.SamplingWindow).mean()
                 self._CalculatedParams.append([col_names[tau],{}])
 
         self._TemporaryData["u_mag" + title_additions] = dir_data["u_mag"]
         self._TemporaryData["u_mag" + title_additions] = self._TemporaryData["u_mag" + title_additions].ffill()
         self._CalculatedParams.append(["u_mag" + title_additions,{}])
-
-        # self._TemporaryData = self._TemporaryData.loc[(self._TemporaryData.index >= self.Identifier["start"]) &
-        #                                               (self._TemporaryData.index < self.Identifier["end"])]
 
         return self
 
