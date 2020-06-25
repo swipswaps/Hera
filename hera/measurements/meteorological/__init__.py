7bf70f9d566e73d6737dc5e948c1f51ce5d6d710
eden <edenn@iibr.gov.il>
2020-06-25 14:55:52 +0300

    - in meteorological - refactor analytics/statistics into analystics/dailyClimatology - creating presentationlayer package and creating dailyClimatology module with david classes

--- hera/measurements/meteorological/__init__.py
+++ hera/measurements/meteorological/__init__.py
@@ -3,7 +3,7 @@ from .datalayer.datalayer import getTurbulenceCalculator
 # from .imsdata import getDocFromFile as IMS_getDocFromFile
 # from .imsdata import getDocFromDB   as IMS_getDocFromDB
 
-from .analytics.statistics import calcHourlyDist
+from .analytics.dailyClimatology import calcHourlyDist
 
 from .imsdata import DailyPlots
 IMS_dailyplots = DailyPlots()
