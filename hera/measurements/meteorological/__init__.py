4a181042a8c119cd6f8a7cb41f9876f87c0443ee
eden <edenn@iibr.gov.il>
2020-08-10 11:24:23 +0300

    - Added CampbellBinaryInterface to meteorological __init__.py - Moved producerCampbellBinary to pyargos

--- hera/measurements/meteorological/__init__.py
+++ hera/measurements/meteorological/__init__.py
@@ -6,6 +6,8 @@ IMS_datalayer = DataLayer_IMS()
 from .datalayer import DataLayer_CampbellBinary
 CampbellBinary_datalayer = DataLayer_CampbellBinary()
 
+from .parserClasses import CampbellBinaryInterface
+
 # from .imsdata import getDocFromFile as IMS_getDocFromFile
 # from .imsdata import getDocFromDB   as IMS_getDocFromDB
 
