7569ca47aa43cbe1e809dfe0c6bf83ff68afd2e7
yehudaa <yehudaa@iibr.gov.il>
2020-08-30 14:48:51 +0300

    0.6.1 -----
    Fixing the indentation at the interpolations.

--- hera/__init__.py
+++ hera/__init__.py
@@ -1,21 +1,4 @@
-__version__ = '0.5.0'
-import json
-import os
-import logging
-import logging.config
-
-
-with open(os.path.join(os.path.dirname(__file__),'logging','heraLogging.config'),'r') as logconfile:
-    log_conf = json.load(logconfile)
-logging.config.dictConfig(log_conf)
-
-EXECUTION = 15
-logging.addLevelName(EXECUTION, 'EXECUTION')
-
-def execution(self, message, *args, **kws):
-    self.log(EXECUTION, message, *args, **kws)
-
-logging.Logger.execution = execution
+__version__ = '0.6.1'
 
 
 import sys
@@ -27,10 +10,23 @@ if version==3:
     from .simulations import LSM
     from .simulations.LSM.DataLayer import SingleSimulation
     from .measurements import GIS
-    from .simulations.interpulations.interpulations import interpulations
+    from .simulations.interpolations.interpolations import spatialInterpolate
 from .simulations import openfoam
 
 """
+ 0.6.1
+ -----
+  - Fixing the simulations.interpolations package. 
+  - Renanimg interpolation->spatialInterpolations.  
+  
+ 0.6.0
+ -----
+  - adding tonumber,tounum and tometeorological/to mathematical functions to the utils. 
+
+ 0.5.1
+ -----
+ CampbellBinary parser and datalayer fixed.
+
  0.5.0
  -----
  Changed the meteorological structure(datalayer and presentaionlayer)
