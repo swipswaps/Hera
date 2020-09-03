066e55012a632343657b34f69a113de2f8c85d3c
yehudaa <yehudaa@iibr.gov.il>
2020-09-03 08:42:43 +0300

    0.7.0 -----
    Adding the riskassessment and the gaussian modules.

---
+++ hera/riskassessment/agents/effects/__init__.py
@@ -0,0 +1,25 @@
+__version__ = (0,0,2)
+
+"""
+0.0.2
+-----
+   * Added the breathing rate considarations. 
+
+0.0.1 
+----- 
+A package to calculate the doasge and project them on 
+geographical area (+ calculating the casualties).
+
+  * Defines the 3 tyes of thresholds: 
+     - Threshold 
+     - Lognormal dose response 
+     - Exponential dose response. 
+
+  * Define 3 types of calculators: 
+     - Haber 
+     - TenBerge. 
+     - MaxConcentration. 
+"""
+
+from pyriskassessment.agents.effects.Injury import InjuryFactory 
+injuryfactory = InjuryFactory()
