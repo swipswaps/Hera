90e689641a2202ebc82881a9019fd8560e84a2af
yehudaa <yehudaa@iibr.gov.il>
2020-11-26 10:47:18 +0200

    Adding agentHome as an interface to create, load and list agents.

--- hera/riskassessment/agents/Agents.py
+++ hera/riskassessment/agents/Agents.py
@@ -5,8 +5,93 @@ from ...utils import tonumber,tounum
 from ...datalayer import ProjectMultiDBPublic
 
 
+class AgentHome(ProjectMultiDBPublic):
+	"""
+		A class to load and get agents.
 
-class Agent(ProjectMultiDBPublic):
+		Supports retrieval from the DB and initializing from a descriptor.
+
+	"""
+
+	def __init__(self, projectName="AgentsCollection"):
+		super().__init__(projectName=projectName, publicProjectName="AgentsCollection")
+
+
+
+	def getAgent(self, nameOrDesc):
+		"""
+			Initialize the agents.
+
+		:param nameOrDesc: str or JSON.
+			Can be either the name of the agent (str) or
+			the descriptor
+
+			{
+				"name" : [the name of the agent],
+				"effectParameters" : {
+					TenBergeCoefficient and ect.
+				},
+				"effects": {
+					"effect name" : { effect data (+ injury levels) }
+
+
+				}
+			}
+
+
+		:param projectName: str
+				The name of the project in the local DB that will be searched for the agent.
+		:return:
+		"""
+		if isinstance(nameOrDesc,str):
+			configList = self.getMeasurementsDocuments(type='Agent',name=nameOrDesc)
+			if len(configList)==0:
+				raise ValueError(f"Agent {nameOrDesc} is not found. Load it with hera-risk-agent load")
+			descriptor = configList.desc
+		elif isinstance(nameOrDesc,dict):
+			descriptor = nameOrDesc
+		else:
+			raise ValueError("nameOrDesc must be the agent name (str) or its JSON description (dict) ")
+
+		return Agent(descriptor)
+
+
+	def listAgents(self):
+		"""
+			Lists the agents that are currently loaded in the DB (both local and public).
+
+		:return: list
+			A list of agent names.
+
+		"""
+		configList = self.getMeasurementsDocuments(type='Agent')
+		return [x['name'] for xin configList]
+
+
+	def loadAgent(self,name,agentDescription,public=True):
+		"""
+			Adds the agent to the DB. Either to the public or to the local DB.
+
+		:param name: str
+				Agent name
+		:param agentDescription: dict
+				The agent description
+
+		:return:
+				None
+		"""
+
+		agentDescription['name'] = name
+
+		destDB = "public" if public else None
+
+		self.addMeasurementsDocument(resource="",
+									 type="Agent",
+									 dataFormat="string",
+									 users=destDB ,
+									 desc=agentDescription)
+
+class Agent:
 
 	_effects = None 
 
@@ -40,30 +125,33 @@ class Agent(ProjectMultiDBPublic):
 		for effectname,effectconfig in self._agentconfig["effects"].items():
 			self._effects[effectname] = injuryfactory.getInjury(effectname,effectconfig,**self._effectParameters)
 
-	def __init__(self,name, projectName="AgentsCollection"):
+
+	def __init__(self,descriptor):
 		"""
-			Reads the agent json. 
-			The name of the agent is the configuration file. 
+			Constructor of the Agent project.
+
+			Initializes an agent.
+
+
+		Parameters
+		-----------
+		descriptor: JSON
+			A JSON object (dict) that holds all the information on the agent.
 
 			{
-				"effectParameters" : { 
-					TenBergeCoefficient and ect. 
+				"name" : [the name of the agent],
+				"effectParameters" : {
+					TenBergeCoefficient and ect.
 				},
-				"effects": { 
-					"effect name" : { effect data (+ injury levels) } 
-					
+				"effects": {
+					"effect name" : { effect data (+ injury levels) }
+
 
 				}
 			}
+
 		"""
-		super().__init__(projectName=projectName,publicProjectName="AgentsCollection")
-
-
-		configList = self.getMeasurementsDocuments(type='Agent',name=name)
-		if len(configList)==0:
-			raise ValueError(f"Agent {name} is not found. Load it with hera-risk-agent load")
-
-		self._agentconfig = configList.desc['agentConfig']
+		self._agentconfig = descriptor['agentConfig']
 		self._effectParameters = self._agentconfig.get("effectParameters",{})
 
 		self._effects = {}
@@ -74,29 +162,6 @@ class Agent(ProjectMultiDBPublic):
 
 		self._physicalproperties = PhysicalPropeties(self._agentconfig)
 
-
-	def loadAgent(self,name,agentDescription,public=True):
-		"""
-			Adds the agent to the DB. Either to the public or to the local DB.
-
-		:param name: str
-				Agent name
-		:param agentDescription: dict
-				The agent description
-
-		:return:
-				None
-		"""
-
-		agentDescription['name'] = name
-
-		destDB = "public" if public else None
-
-		self.addMeasurementsDocument(resource="",
-									 type="Agent",
-									 dataFormat="string",
-									 users=destDB ,
-									 desc=agentDescription)
 
 
 
